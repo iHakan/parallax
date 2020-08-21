30becf5c8ea83b54af01ced65f02ce079aa42f31
iHakan <ihakan@yaani.com>
2020-08-21 14:46:14 +0300

    Headers are centered now

--- src/style.js
+++ src/style.js
@@ -13,26 +13,16 @@ const style = StyleSheet.create({
     width: '100%',
     height: '100%',
   },
-  text: {
-    width: w*0.7,
-    color: '#eee',
-    fontSize: 50,
-    position:'relative',
-    fontWeight:'bold',
-    margin: 'auto',
-  },
-  con: {
-    flex: 1,
-    padding: 0,
-    marginBottom: 0,
-    //borderWidth: 1,
-    alignItems: 'center',
-    borderColor: '#eee',
-  },
   imageBackground: {
     marginBottom: 5,
-    width: w * 1.5,
+    width: w * 1,
     height: h * 0.5,
+    alignItems:'center'
+  },
+  text: {
+    color: '#eee',
+    fontSize: 50,
+    fontWeight:'bold',
   },
 });
 
